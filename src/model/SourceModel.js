e9505e58dcb4bebcfd76aed4cca237d7b0a072bc
lamba <lam45883@gmail.com>
2025-03-22 03:34:09 +0700

    Optimize product, deal, customer

--- src/model/SourceModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const SourceSchema = new mongoose.Schema({
-  name: String,
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-});
-const SourceModel = mongoose.model("Source", SourceSchema);
-export default SourceModel;
